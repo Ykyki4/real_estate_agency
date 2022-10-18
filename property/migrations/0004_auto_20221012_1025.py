0f2514f21d929338ca9ced2707ed42babe084f52
Ykyki4 <jimmypantalones@gmail.com>
2022-10-18 11:59:04 +0300

    Move shortened migration

--- property/migrations/0004_auto_20221012_1025.py
+++ property/migrations/0004_auto_20221012_1025.py
@@ -4,19 +4,14 @@ from django.db import migrations
 
 def add_value_to_new_building(apps, schema_editor):
     Flat = apps.get_model('property', 'Flat')
-    new_building_flats = Flat.objects.filter(construction_year__gte=2015)
-    old_building_flats = Flat.objects.filter(construction_year__lte=2014)
-    for new_building_flat in new_building_flats:
-        new_building_flat.new_building = True
-        new_building_flat.save()
-    for old_building_flat in old_building_flats:
-        old_building_flat.new_building = False
-        old_building_flat.save()
+    Flat.objects.filter(construction_year__gte=2015).update(new_building=True)
+    Flat.objects.filter(construction_year__lte=2014).update(new_building=False)
+
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('property', '0003_flat_new_building'),
+        ('property', '0015_auto_20221013_0859'),
     ]
 
     operations = [
